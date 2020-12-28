100a42b0b47de89c2da139c991e1290b955a73be
juan carrero <juancarrero811@gmail.com>
2020-12-28 14:46:52 -0430

    se agrego el favicon

--- components/PageLayout/index.js
+++ components/PageLayout/index.js
@@ -11,6 +11,10 @@ export function PageLayout ({ title = 'Uchurita', children }) {
         <title>{title}</title>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel='icon' type='image.png' sizes='32x32' href='/favicon32x32.png' />
+        <link rel='icon' type='image.png' sizes='16x16' href='/favicon16x16.png' />
+        <link rel='apple-touch-icon' sizes='180x180' href='apple-touch-icon.png' />
+        <meta name='theme-color' content='#141414' />
       </Head>
       <Header />
       <main className={styles.main}>
